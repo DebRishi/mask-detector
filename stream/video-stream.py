0b72cb2ed372d166c88b3cd73c67a422518ea244
dmallick <dmallick@informatica.com>
2023-09-11 16:42:37 +0530

    Improved project structure

--- stream/video-stream.py
+++ stream/video-stream.py
@@ -59,12 +59,12 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 	return (locs, preds)
 
 # load our serialized face detector model from disk
-prototxtPath = r"face_detector\deploy.prototxt"
-weightsPath = r"face_detector\res10_300x300_ssd_iter_140000.caffemodel"
+prototxtPath = r"../model/face_detector/deploy.prototxt"
+weightsPath = r"../model/face_detector/res10_300x300_ssd_iter_140000.caffemodel"
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
 # load the face mask detector model from disk
-maskNet = load_model("mask_detector.model")
+maskNet = load_model("../model/mask_detector.model")
 
 # initialize the video stream
 print("[INFO] starting video stream...")
